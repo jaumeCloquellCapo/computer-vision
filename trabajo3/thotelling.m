ce6f50c0281f445b3aa8955083be5bf5ea2bba39
jaume cloquell <jaumecloquell@hotmail.com>
2019-05-28 18:22:01 +0200

    hotelling p4

--- trabajo3/thotelling.m
+++ trabajo3/thotelling.m
@@ -1,30 +1,18 @@
-%Read images
-selection = 2;
-
-if (selection == 1)
-    for n = 1:6
-        path = strcat('adra/banda', int2str(n), '.tif');
-        adraImg = imread(path);
-        adraImg = im2double(adraImg);
-        adraImages(:,:,n) = adraImg;
-    end
-else
-    index = 1;
-    for n = 'a':'e'
-        path = strcat('camiones/', n, '.jpg');
-        adraImg = imread(path);
-        adraImg = rgb2gray(adraImg);
-        adraImg = im2double(adraImg);
-        adraImages(:,:,index) = adraImg;
-        index = index + 1;
-    end
+%Lecura de las imagenes
+index = 1;
+for n = 'a':'e'
+    path = strcat('camiones/', n, '.jpg');
+    adraImg = imread(path);
+    adraImg = rgb2gray(adraImg);
+    adraImg = im2double(adraImg);
+    adraImages(:,:,index) = adraImg;
+    index = index + 1;
 end
+
 sizeVector = size(adraImages);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Applu Hotelling transform
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%Hotelling
 %Mean and expectation mean  of pixel vectors
 mean = zeros(1,sizeVector(3), 'double');
 for i = 1:sizeVector(1)
@@ -34,10 +22,10 @@ for i = 1:sizeVector(1)
         mean = mean + vector;
     end
 end
+
 mean = mean / (sizeVector(1) * sizeVector(2));
 expectationMean = ((mean') * mean) / (sizeVector(1) * sizeVector(2));
 
-%Expectation of pixel vectors
 expectation = zeros(sizeVector(3),sizeVector(3), 'double');
 for i = 1:sizeVector(1)
     for j = 1:sizeVector(2)
@@ -48,17 +36,17 @@ for i = 1:sizeVector(1)
 end
 expectation = expectation / (sizeVector(1) * sizeVector(2));
 
-%Covariance matrix
+%Matriz de Covarianza
 covMatrix = expectation - expectationMean;
 
-%Get Eigenvalues and Eigenvectors
+%Obtenemos los Autovalores y Autovectores
 [eigenVectors, eigenValues] = eig(covMatrix);
 
-%Order eigenVectors by eigenValues
+%Ordenamos los Autovectores por los Autovalores
 eigenVectors = fliplr(eigenVectors);
 eigenValues = flipud(fliplr(eigenValues));
 
-%Apply Hotelling on pixel vectors y = A(x-m)
+% Aplicamos sobre los Autovectores la transformada de la siguiente forma y=A(x?mx) para descubrir las nuevas imágenes, 
 newAdraImages = adraImages;
 for n = 1:sizeVector(3)
     for i = 1:sizeVector(1)
@@ -70,10 +58,10 @@ for n = 1:sizeVector(3)
     end
 end
 
-figure, subplot(2,3,1), imshow(newAdraImages(:,:,1)),...
-    subplot(2,3,2), imshow(newAdraImages(:,:,2), []),...
-    subplot(2,3,3), imshow(newAdraImages(:,:,3), []),...
-    subplot(2,3,4), imshow(newAdraImages(:,:,4), []),...
-    subplot(2,3,5), imshow(newAdraImages(:,:,5), []),...
-    subplot(2,3,6), imshow(newAdraImages(:,:,6), []);
+figure, 
+subplot(2,3,1),imshow(newAdraImages(:,:,1));
+subplot(2,3,2), imshow(newAdraImages(:,:,2), []);
+subplot(2,3,3), imshow(newAdraImages(:,:,3), []);
+subplot(2,3,4), imshow(newAdraImages(:,:,4), []);
+subplot(2,3,5), imshow(newAdraImages(:,:,5), []);
     
