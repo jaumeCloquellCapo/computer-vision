ebc219bd556a0188523217a645dd08e45a524ccc
jaume cloquell <jaumecloquell@hotmail.com>
2019-06-03 22:31:57 +0200

    trabajo2

--- trabajo2/trabajo2.m
+++ trabajo2/trabajo2.m
@@ -1,15 +1,31 @@
-% Leemos la imagen
-castillo_orig = imread('castillo.pgm');
-castillo_vac = imread('Castillo_oculto2bits.pgm');
+castillo = imread('castillo.pgm');
+Castillo_oculto2bits = imread('Castillo_oculto2bits.pgm');
 
-% desencriptar
-A = castillo_vac;
-figure, subplot(2,2,1);imshow(castillo_vac);title('castillo con imagen oculta');
-B=bitget(A,1); subplot(2,2,2);imshow(logical(B));title('Primer imagen '); %obtenos el primer bit
-B=bitget(A,2); subplot(2,2,3);imshow(logical(B));title('Segunda imagen'); %obtenos el segundo bit
-% encriptar
-B = bitshift(castillo_orig,-1);
+% realizamos una operación AND para de una forma sencilla, permite comparar
+% los bits que le indiquemos. Si comparten ponemos una mascara de 1 por
+% ejemplo, este en 8 bits vendría representado por 1 0 0 0 0 0 0 0 .
+% Entonces en este caso si comparten 1 en la misma posición que la imagen
+% el resultado es 1 en caso contrrio 0. De esta forma obtememos el bit
+% menos significativo duna forma rápida y sencilla.
+
+% 1 => 1 0 0 0 0 0 0 0 en 8 bits
+vacas_2 = bitand(Castillo_oculto2bits,1);
+
+% 3 => 0 1 0 0 0 0 0 0 en 8 bits
+vacas_3 = bitand(Castillo_oculto2bits,3);
+vacas_3 = double(vacas_3);
+vacas_3 = (vacas_3 - min(min(vacas_3))) / (max(max(vacas_3)) - min(min(vacas_3)));
+
+
+figure,subplot(1,4,1),imshow(Castillo_oculto2bits), title('Original');
+subplot(1,4,2),imshow(double(vacas_2)), title('Vaca 2 bits');
+subplot(1,4,3),imshow(vacas_3), title('Vaca 3 bits');
+
+
+% Para recuperar la original encriptada desplazamos el bit más
+% significativo y luego desplazamos todo a la izquierda para restar 1 a la
+% imagen y sumamos los niveles de gris 
+B = bitshift(castillo,-1);
 B = bitshift(B,1);
-B = B + bitand(castillo_vac,1);
-subplot(2,2,4);imshow(B);title('Imagen encriptada');
-
+B = B + bitand(Castillo_oculto2bits,1);
+subplot(1,4,4),imshow(B), title('Imagen encriptada');
--- trabajo4/acquire.m
+++ trabajo4/acquire.m
@@ -1,4 +1,4 @@
-function [newdata, y, v, FrameRate] = acquire(video_file)
+function [newdata, y, v, FrameRate, mse] = acquire(video_file)
 if ischar(video_file),
     display(['Loading file ' video_file]);
     v = VideoReader(video_file);
@@ -10,6 +10,7 @@ numFrames = v.NumberOfFrames;
 
 display(['Total frames: ' num2str(numFrames)]);
 
+% Generamos tres vectores (uno por banda) 
 y = zeros(1, numFrames);
 x = zeros(1, numFrames);
 z = zeros(1, numFrames);
@@ -17,6 +18,7 @@ z = zeros(1, numFrames);
 for i=1:numFrames,
     display(['Processing ' num2str(i) '/' num2str(numFrames)]);
     frame = read(v, i);
+    % Tomamos las tres bandas
     y(i) = sum(sum(frame(:, :, 1))) / (size(frame, 1) * size(frame, 2)); 
     x(i) = sum(sum(frame(:, :, 2))) / (size(frame, 1) * size(frame, 2)); 
     z(i) = sum(sum(frame(:, :, 3))) / (size(frame, 1) * size(frame, 2));
@@ -27,13 +29,13 @@ yi = (y - mean(y)) ./ std(y);
 xi = (x - mean(x)) ./ std(x);
 zi = (z - mean(z)) ./ std(z);
 
-data = transpose([yi; xi; zi]);
+data = [yi; xi; zi];
 
 %PCA
 
 % A continuación calculamos la matriz de covarianza de los datos. 
 % La matriz de covarianza contiene toda la información necesaria para rotar el sistema de coordenadas.
-C = cov(data);
+C = cov(data');
 
 % Los valores propios D de la matriz de covarianza, es decir, la versión diagonal de C, da la varianza dentro de los nuevos ejes de coordenadas, es decir, los componentes principales. Ahora calculamos los vectores propios V y los valores propios D de la matriz de covarianza C.
 [V,D] = eig(C);
@@ -41,13 +43,15 @@ C = cov(data);
 V = V(:,order);
 
 % Cálculo del conjunto de datos en el nuevo sistema de coordenadas. Necesitamos voltear los datos nuevos hacia la izquierda / derecha ya que la segunda columna es la que tiene el valor propio más grande.
-newdata = data * V(:,1);
+newdata = data' * V(:,1);
 newdata = newdata';
+mse = immse(newdata,y);
 FrameRate = v.FrameRate;
 
 display('Signal acquired.');
 display(' ');
 display(['Sampling rate is ' num2str(v.FrameRate) '. You can now run process(your_signal_variable, ' num2str(v.FrameRate) ')']);
+display(['MSE: ' num2str(mse)]);
 
 end
 
